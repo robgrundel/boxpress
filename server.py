8640b1307ebd505036d4acdc3811638bc9c3d8cf
Rob Grundel <rob@mingus.home>
2013-05-28 22:52:17 +0100

    Now with metadata at the start of the blog article

--- server.py
+++ server.py
@@ -2,18 +2,38 @@ import cherrypy
 import os.path
 import codecs
 import markdown
+import re
 from mako.template import Template
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Boxpress:
+  
   def index(self):
     input_file = codecs.open("how-I-wrote-this-blog.md", mode="r", encoding="utf-8")
-    t = input_file.read()
+
+    metadata = ''
+    for x in range(0, 3):
+	  metadata = metadata + input_file.readline()
+    
+    title = self.read_metadata(metadata, 'title')
+    date =  self.read_metadata(metadata, 'date')
+    tags = self.read_metadata(metadata, 'tags')
+
+    t = ''
+    for line in input_file:
+    	t = t + line
     html = markdown.markdown(t)
+
     template = Template(filename='index.html')	
-    return template.render(content=html)
+    
+    return template.render(content=html, title=title, date=date, tags=tags)
   index.exposed = True
+  
+  def read_metadata(self, source, key):
+    m = re.search('(?<=' + key + ':).*', source)
+    return m.group(0)
+  	 
 
 if __name__ == '__main__':
   current_dir = os.path.dirname(os.path.abspath(__file__))

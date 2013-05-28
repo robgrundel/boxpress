5df9fb246250e1640cce40bb4a9b4327199ed7f8
Rob Grundel <rob@mingus.home>
2013-05-28 23:51:37 +0100

    not sure about this but have Dropbox Authentication sorted. Maybe worth putting on a seperate url

--- server.py
+++ server.py
@@ -3,32 +3,57 @@ import os.path
 import codecs
 import markdown
 import re
+from dropbox import client, rest, session
 from mako.template import Template
+
+APP_KEY = 'dzqhhfga39e41xf'
+APP_SECRET = '8d2abh3qjg2epwk'
+
+ACCESS_TYPE = 'app_folder'
+sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Boxpress:
   
+  request_token = ''
+
+  def set_dropbox_auth(self, oauth_token, uid):
+  	print oauth_token
+  	print uid
+  	access_token = sess.obtain_access_token(self.request_token)
+  	raise cherrypy.HTTPRedirect("/")
+  
+  	
+  
   def index(self):
-    input_file = codecs.open("how-I-wrote-this-blog.md", mode="r", encoding="utf-8")
-
-    metadata = ''
-    for x in range(0, 3):
-	  metadata = metadata + input_file.readline()
+    try:	
+      box_client = client.DropboxClient(sess)
+      post, m = box_client.get_file_and_metadata('how-I-wrote-this-blog.md')
+    except Exception:
+      self.request_token = sess.obtain_request_token()
+      url = sess.build_authorize_url(self.request_token, oauth_callback='http://127.0.0.1:5000/set_dropbox_auth')
+      raise cherrypy.HTTPRedirect(url)
     
-    title = self.read_metadata(metadata, 'title')
-    date =  self.read_metadata(metadata, 'date')
-    tags = self.read_metadata(metadata, 'tags')
+    
+    # metadata = ''
 
-    t = ''
-    for line in input_file:
-    	t = t + line
+    # for x in range(0, 3):
+	 # metadata = metadata + input_file.readline()
+    
+    #title = self.read_metadata(metadata, 'title')
+    #date =  self.read_metadata(metadata, 'date')
+    #tags = self.read_metadata(metadata, 'tags')
+    t = post.read()
+    
     html = markdown.markdown(t)
 
     template = Template(filename='index.html')	
     
-    return template.render(content=html, title=title, date=date, tags=tags)
+    return template.render(content=html, title='title', date='date', tags='tags')
   index.exposed = True
+  set_dropbox_auth.exposed = True
   
   def read_metadata(self, source, key):
     m = re.search('(?<=' + key + ':).*', source)

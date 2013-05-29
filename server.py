a5cc37c08c92188e1d9896ca5670f806a5c33030
Rob Grundel <rob@mingus.home>
2013-05-30 00:32:12 +0100

    can we get static images working on Heroku

--- server.py
+++ server.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
   conf = {
 		'global' :  {'server.socket_host': '0.0.0.0', 'server.socket_port': int(os.environ.get('PORT', '5000'))},
 		'/': {'tools.staticdir.root':  current_dir},
-		'/static': {'tools.staticdir.on':  True,'tools.staticdir.dir': 'static'}
+		'/static': {'tools.staticdir.on':  True,'tools.staticdir.dir': './static'}
 		}
 
 dropbox_session = DropboxSession()
